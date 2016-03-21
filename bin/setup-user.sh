d66cef8d7d08fd822b79f9c7f3c59a9c54a78e05
Tai Lee <real.human@mrmachine.net>
2016-03-21 11:33:54 +1100

    If the given home directory already exists and is owned by root, change its UID and GID to match the unprivileged user.

--- bin/setup-user.sh
+++ bin/setup-user.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Create a system user with the given username and home directory. If the user
-# already exists, ensure its UID and GID match the given directory.
+# Create an unprivileged user with the given username and home directory. If
+# the user already exists, change its UID and GID match the given home
+# directory. If the home directory already exists and is owned by root, change
+# its UID and GID to match the unprivileged user.
 
 set -e
 
@@ -18,12 +20,19 @@ if ! id "${USERNAME}" 2> /dev/null; then
     fi
 fi
 
-# Modify user.
+# Get UID and GID for user and home directory.
 HOME_DIR_GID=$(stat -c '%g' "${HOME_DIR}")
 HOME_DIR_UID=$(stat -c '%u' "${HOME_DIR}")
 USER_UID=$(id -u "${USERNAME}")
 USER_GID=$(id -g "${USERNAME}")
+
+# Change user or home directory UID and GID.
 if [[ "${USER_UID}" != "${HOME_DIR_UID}" ]] || [[ "${USER_GID}" != "${HOME_DIR_GID}" ]]; then
-    echo "UID and GID for user '${USERNAME}' (${USER_UID}:${USER_GID}) do not match directory '${HOME_DIR}' (${HOME_DIR_UID}:${HOME_DIR_GID}). Modify."
-    usermod -g "${HOME_DIR_GID}" -u "${HOME_DIR_UID}" "${USERNAME}"
+	if [[ "${HOME_DIR_UID}" == 0 ]]; then
+		echo "Unprivileged user home directory '${HOME_DIR}' is owned by root. Change owner."
+		chown -R "${USER_UID}:${USER_GID}" "${HOME_DIR}"
+	else
+		echo "UID and GID for user '${USERNAME}' (${USER_UID}:${USER_GID}) do not match directory '${HOME_DIR}' (${HOME_DIR_UID}:${HOME_DIR_GID}). Modify."
+		usermod -g "${HOME_DIR_GID}" -u "${HOME_DIR_UID}" "${USERNAME}"
+	fi
 fi
