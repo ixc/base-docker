e1df7b8d14e6333a6389483fe46c297e7856df77
Tai Lee <real.human@mrmachine.net>
2016-04-02 21:28:56 +1100

    Refactor `entrypoint-base.sh` as `entrypoint-gosu-dir.sh`, which more clearly describes its purpose.

---
+++ bin/entrypoint-gosu-dir.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Execute a command with the same UID and GID as the given directory.
+
+set -e
+
+DIR="$1"
+
+if [[ ! -d "${DIR}" ]]; then
+	echo "Directory '${DIR}' does not exist. Abort."
+	exit 1
+fi
+
+DIR_GID=$(stat -c '%g' "${DIR}")
+DIR_UID=$(stat -c '%u' "${DIR}")
+
+exec gosu "${DIR_UID}:${DIR_UID}" "${@:2}"
