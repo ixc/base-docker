5f55085284fabe0d2a81328ffbcdb74cf98cbc03
Tai Lee <real.human@mrmachine.net>
2016-04-07 12:07:00 +1000

    Non-verbose output for wget.

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,19 +14,19 @@ fi
 
 # Dockerize.
 DOCKERIZE_VERSION=0.2.0
-wget -O - "https://github.com/jwilder/dockerize/releases/download/v${DOCKERIZE_VERSION}/dockerize-linux-amd64-v${DOCKERIZE_VERSION}.tar.gz" | tar -xz -C /usr/local/bin/ -f -
+wget -nv -O - "https://github.com/jwilder/dockerize/releases/download/v${DOCKERIZE_VERSION}/dockerize-linux-amd64-v${DOCKERIZE_VERSION}.tar.gz" | tar -xz -C /usr/local/bin/ -f -
 
 # Gosu.
 GOSU_VERSION=1.7
-wget -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-$(dpkg --print-architecture)"
+wget -nv -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-$(dpkg --print-architecture)"
 chmod +x /usr/local/bin/gosu
 
 # Scripts.
 mkdir -p /opt/base/bin
 cd /opt/base/bin
-wget "https://raw.githubusercontent.com/ixc/base-docker/${COMMIT}/bin/entrypoint-gosu-dir.sh"
-wget "https://raw.githubusercontent.com/ixc/base-docker/${COMMIT}/bin/gulp.sh"
-wget "https://raw.githubusercontent.com/ixc/base-docker/${COMMIT}/bin/setup-postgres.sh"
-wget "https://raw.githubusercontent.com/ixc/base-docker/${COMMIT}/bin/setup-user.sh"
+wget -nv "https://raw.githubusercontent.com/ixc/base-docker/${COMMIT}/bin/entrypoint-gosu-dir.sh"
+wget -nv "https://raw.githubusercontent.com/ixc/base-docker/${COMMIT}/bin/gulp.sh"
+wget -nv "https://raw.githubusercontent.com/ixc/base-docker/${COMMIT}/bin/setup-postgres.sh"
+wget -nv "https://raw.githubusercontent.com/ixc/base-docker/${COMMIT}/bin/setup-user.sh"
 chmod +x /opt/base/bin/*.sh
 export PATH=/opt/base/bin:$PATH
